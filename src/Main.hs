d670439af3d414100e7a02468e54bd81736fb346
Devin Jacobs <dbuttonz@gmail.com>
2018-03-31 16:03:35 -0600

    Gtk Hello World

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,21 @@
-main = putStrLn "hello world"
+import qualified GI.Gtk as Gtk
+import GI.Gtk hiding (main)
+import GI.Gtk.Enums (WindowType(..))
+
+main = do
+    _ <- Gtk.init Nothing
+
+    window <- windowNew WindowTypeToplevel
+    _ <- onWidgetDestroy window mainQuit
+    setContainerBorderWidth window 10
+    setWindowTitle window "Hello World"
+
+    button <- buttonNew
+    setButtonLabel button "Hello World"
+    _ <- onButtonClicked button $
+        putStrLn "Hello World"
+
+    setContainerChild window button
+    widgetShowAll window
+
+    Gtk.main
