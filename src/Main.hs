3e24c58484c943388bc85790bbc202abcce4855e
Devin Jacobs <dbuttonz@gmail.com>
2018-04-08 17:09:48 -0600

    Draw something/anything on a drawing area

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,12 @@
+import Control.Monad.Trans.Reader (runReaderT)
+import Foreign.Ptr (castPtr)
 import qualified GI.Gtk as Gtk
 import GI.Gtk hiding (main)
 import GI.Gtk.Enums (WindowType(..))
+import qualified GI.Cairo
+import Graphics.Rendering.Cairo
+import Graphics.Rendering.Cairo.Internal (Render(runRender))
+import Graphics.Rendering.Cairo.Types (Cairo(Cairo))
 
 main = do
     _ <- Gtk.init Nothing
@@ -10,12 +16,25 @@ main = do
     setContainerBorderWidth window 10
     setWindowTitle window "Hello World"
 
-    button <- buttonNew
-    setButtonLabel button "Hello World"
-    _ <- onButtonClicked button $
-        putStrLn "Hello World"
+    -- button <- buttonNew
+    -- setButtonLabel button "Hello World"
+    -- _ <- onButtonClicked button $
+    --     putStrLn "Hello World"
 
-    setContainerChild window button
+    boardDrawingArea <- drawingAreaNew
+    _ <- onWidgetDraw boardDrawingArea $ \context -> do
+        renderWithContext context $ do
+            setSourceRGB 0 2000 0
+            paint
+        return True
+
+
+    setContainerChild window boardDrawingArea
     widgetShowAll window
 
     Gtk.main
+
+renderWithContext :: GI.Cairo.Context -> Render () -> IO ()
+renderWithContext context r =
+    withManagedPtr context $ \p ->
+    runReaderT (runRender r) (Cairo (castPtr p))
